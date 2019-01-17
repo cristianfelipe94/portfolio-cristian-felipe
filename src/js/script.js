21f44337402cd92edc8c59beb24439ba6ea6aa43
Cristian Felipe <cristianpipe0494@gmail.com>
2019-01-16 18:11:09 -0600

    Have added transition to landing page and tabs menu. Still need to organize and change variable names.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
-/**
- * Project Description.
- * ...
- */
+const helloBtn = document.getElementById('js-hello');
+const landingPage = document.getElementById('js-landingpage');
+const VerticalTabs = document.getElementById('js-tabs-info-wrapper');
+const btnClose = document.getElementById('js-btn-close');
 
-const text = 'Hola mundo!';
-const container = document.getElementById('container');
-const paragraph = document.createElement('p');
-paragraph.innerText = text;
-container.appendChild(paragraph);
+helloBtn.addEventListener('click', function () {
+    landingPage.setAttribute('class', 'landingPageMoves');
+    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-showing');
+});
+
+btnClose.addEventListener('click', function () {
+    console.log('closing');
+    landingPage.setAttribute('class', 'landing-page-wrapper');
+    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-hidden');
+});
