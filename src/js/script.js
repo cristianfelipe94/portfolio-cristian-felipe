e3f5f6f7cdac7fc36c35a714a94eee52c7a4e728
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-13 12:13:26 -0600

    Have change project from multi-page to single-page with tabs functionality. Code is working, but I need to organize it.

--- src/js/script.js
+++ src/js/script.js
@@ -1,80 +1,137 @@
-const helloBtn = document.getElementById('js-hello');
-const landingPage = document.getElementById('js-landingpage');
-const VerticalTabs = document.getElementById('js-tabs-info-wrapper');
-const btnClose = document.getElementById('js-btn-close');
-const contentBlock = document.getElementById('content-block');
+(function () {
+    const helloBtn = document.getElementById('js-hello');
+    const landingPage = document.getElementById('js-landingpage');
+    const VerticalTabs = document.querySelectorAll('#js-tabs-info-wrapper');
+    const btnClose = document.getElementById('js-btn-close');
+    const homeContentBlock = document.getElementById('js-home-page');
 
-const backgroundModal = document.getElementById('js-modal');
-const closeModalBtn = document.getElementById('js-close-modal');
+    const tabsDom = document.getElementsByClassName('tab-items');
 
-const portFolioContent = document.getElementById('js-wrapper-portfolio');
+    function hiddingElements(elementsToBeHide) {
+        elementsToBeHide.className = 'content-page-hidden';
+    }
 
-helloBtn.addEventListener('click', function () {
-    landingPage.setAttribute('class', 'landingPageMoves');
-    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-showing');
-    contentBlock.setAttribute('class', 'content-page-showing');
-    portFolioContent.setAttribute('class', 'content-inside-wrapper-showing');
-});
+    function showingElements(elementsToBeShown) {
+        elementsToBeShown.className = 'content-page-showing';
+    }
 
-btnClose.addEventListener('click', function () {
-    landingPage.setAttribute('class', 'landing-page-wrapper');
-    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-hidden');
-    contentBlock.setAttribute('class', 'content-page-hidden');
-    portFolioContent.setAttribute('class', 'content-inside-wrapper-hidden');
-});
+    const tabsArray = [];
 
-const imgPortArray = [
-    imgPort1 = {
-        src: '../img/canvas_game_port1.jpg',
-        alt: 'canvas mini game picture',
-    },
-    imgPort2 = {
-        src: '../img/derrota_palabra_port1.jpg',
-        alt: 'derrota la palabra game picture',
-    },
-    imgPort3 = {
-        src: '../img/tiny_one_port1.jpg',
-        alt: 'tiny one picture',
-    },
-    imgPort4 = {
-        src: '../img/folio_port1.jpg',
-        alt: 'folio picture',
-    },
-    imgPort5 = {
-        src: '../img/blaz_port1.jpg',
-        alt: 'blaz game picture',
-    },
-    imgPort6 = {
-        src: '../img/cuda_port1.jpg',
-        alt: 'cuda game picture',
-    },
-];
+    function tab(selector) {
+        const elements = selector;
 
-closeModalBtn.addEventListener('click', function () {
-    backgroundModal.setAttribute('class', 'modal-content-hide');
-});
+        for (const element of elements) {
 
-imgPortArray.forEach(element => {
-    const elementSrc = element.src;
-    const elementAlt = element.alt;
-    const elementPort = document.createElement('img');
-    elementPort.setAttribute('src', elementSrc);
-    elementPort.setAttribute('alt', elementAlt);
-    elementPort.setAttribute('class', 'gallery-port');
-    elementPort.addEventListener('click', function () {
+            const links = element.querySelectorAll('ul > li > a');
 
-        const modalElement = document.createElement('img');
+            const listItem = element.querySelectorAll('div');
+            tabsArray.push(listItem);
 
-        modalElement.setAttribute('src', elementSrc);
-        modalElement.setAttribute('class', 'modal-img-display');
+            for (const link of links) {
 
-        backgroundModal.setAttribute('class', 'modal-content-display');
+                link.addEventListener('click', function change(e) {
+                    clear();
+                    let tabsOption = e.target.getAttribute('href');
+                    let tabsContent = element.querySelector(tabsOption);
+                    showingElements(tabsContent);
+                });
 
-        closeModalBtn.addEventListener('click', function () {
-            backgroundModal.setAttribute('class', 'modal-content-hide');
-        });
+                function clear() {
+                    for (const item of listItem) {
+                        hiddingElements(item);
+                    }
+                }
+            }
+        }
+    }
+    tab(VerticalTabs);
+
+    const backgroundModal = document.getElementById('js-modal');
+    const closeModalBtn = document.getElementById('js-close-modal');
+
+    const portFolioContent = document.getElementById('js-wrapper-portfolio');
+
+    helloBtn.addEventListener('click', function () {
         
-        backgroundModal.appendChild(modalElement);
+        landingPage.setAttribute('class', 'landingPageMoves');
+        for (const e of VerticalTabs) {
+            e.setAttribute('class', 'tabs-info-wrapper-showing');
+        };
+
+        showingElements(homeContentBlock);
     });
-    portFolioContent.appendChild(elementPort);
-});
+
+    btnClose.addEventListener('click', function () {
+
+        landingPage.setAttribute('class', 'landing-page-wrapper');
+        for (const e of VerticalTabs) {
+            e.setAttribute('class', 'tabs-info-wrapper-hidden');
+        };
+
+        tabsArray.forEach(element => {
+            for (const i of element) {
+                if (i.className === 'content-page-showing') {
+                    hiddingElements(i);
+                }
+            }
+        });
+
+    });
+
+    const imgPortArray = [
+        imgPort1 = {
+            src: '../img/canvas_game_port1.jpg',
+            alt: 'canvas mini game picture',
+        },
+        imgPort2 = {
+            src: '../img/derrota_palabra_port1.jpg',
+            alt: 'derrota la palabra game picture',
+        },
+        imgPort3 = {
+            src: '../img/tiny_one_port1.jpg',
+            alt: 'tiny one picture',
+        },
+        imgPort4 = {
+            src: '../img/folio_port1.jpg',
+            alt: 'folio picture',
+        },
+        imgPort5 = {
+            src: '../img/blaz_port1.jpg',
+            alt: 'blaz game picture',
+        },
+        imgPort6 = {
+            src: '../img/cuda_port1.jpg',
+            alt: 'cuda game picture',
+        },
+    ];
+
+    closeModalBtn.addEventListener('click', function () {
+        backgroundModal.setAttribute('class', 'modal-content-hide');
+    });
+
+    function createPort() {
+        imgPortArray.forEach(element => {
+            const elementSrc = element.src;
+            const elementAlt = element.alt;
+            const elementPort = document.createElement('img');
+            elementPort.setAttribute('src', elementSrc);
+            elementPort.setAttribute('alt', elementAlt);
+            elementPort.setAttribute('class', 'gallery-port');
+            elementPort.addEventListener('click', function () {
+                const modalElement = document.createElement('img');
+    
+                modalElement.setAttribute('src', elementSrc);
+                modalElement.setAttribute('class', 'modal-img-display');
+    
+                backgroundModal.setAttribute('class', 'modal-content-display');
+    
+                closeModalBtn.addEventListener('click', function () {
+                    backgroundModal.setAttribute('class', 'modal-content-hide');
+                });
+    
+                backgroundModal.appendChild(modalElement);
+            });
+            portFolioContent.appendChild(elementPort);
+        });
+    }
+}());
